f8c8acf2f8c7886604e3344866600f2be1264c49
Theresa Newman <theresa.newman@haymarketmedia.com>
2025-05-20 11:49:09 -0400

    add assert tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 import * as chai from "chai";
 const should = chai.should();
+const assert = chai.assert;
+const expect = chai.expect;
 import Add from "../maths.js";
 
 describe("addition-test", function () {
@@ -8,14 +10,16 @@ describe("addition-test", function () {
     const numberTwo = 2;
     const expectedResult = 3;
     const actualResult = Add(numberOne, numberTwo);
-    actualResult.should.equal(expectedResult);
-    //expect(actualResult).to.equal(expectedResult);
+    // actualResult.should.equal(expectedResult);
+    //expect(actualResult, "expect to equal").to.equal(expectedResult);
+    assert.equal(actualResult, expectedResult, "expect to equal");
   });
   it("should not return 3 when passed 1 and 4", function () {
     const numberOne = 1;
     const numberTwo = 4;
-    const expectedResult = 3;
+    const notExpectedResult = 3;
     const actualResult = Add(numberOne, numberTwo);
-    expect(actualResult).to.not.equal(expectedResult);
+    // expect(actualResult, "expect not to equal").to.not.equal(notExpectedResult);
+    assert.notEqual(actualResult, notExpectedResult, "expect not to equal");
   });
 });
