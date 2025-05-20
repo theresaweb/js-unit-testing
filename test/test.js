ee4e2050a8d905d9cd24d79ad1a86c7b246cecd8
Theresa Newman <theresa.newman@haymarketmedia.com>
2025-05-20 11:39:44 -0400

    using should and expect

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-import { should } from "chai";
+import * as chai from "chai";
+const should = chai.should();
 import Add from "../maths.js";
 
 describe("addition-test", function () {
@@ -8,5 +9,13 @@ describe("addition-test", function () {
     const expectedResult = 3;
     const actualResult = Add(numberOne, numberTwo);
     actualResult.should.equal(expectedResult);
+    //expect(actualResult).to.equal(expectedResult);
+  });
+  it("should not return 3 when passed 1 and 4", function () {
+    const numberOne = 1;
+    const numberTwo = 4;
+    const expectedResult = 3;
+    const actualResult = Add(numberOne, numberTwo);
+    expect(actualResult).to.not.equal(expectedResult);
   });
 });
